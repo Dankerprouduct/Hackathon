eb8474bf0fba817b2e4a2563442bdf330def0b54
David <Dankerprouduct2@gmail.com>
2015-10-14 23:17:37 -0400

    Armor Piece Class
    base class for character custimization

---
+++ ArmorPieces.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmorPieces : MonoBehaviour {
+
+
+    public string pieceName;
+    public int itemID;
+    public ArmorType armor;
+    public float speed;
+    public float health; 
+
+    public enum ArmorType
+    {
+        Helmet, 
+        Torso, 
+        RightForearm,
+        LeftForearm,
+        RightBicep,
+        LeftBicep,
+        RightFoot,
+        LeftFoot
+    }
+
+    public ArmorPieces(string name, int id, ArmorType type, float itemSpeed, float itemHealth)
+    {
+        pieceName = name;
+        itemID = id;
+        armor = type;
+        speed = itemSpeed;
+        health = itemHealth;
+    }
+
+    public ArmorPieces()
+    {
+
+    }
+}
