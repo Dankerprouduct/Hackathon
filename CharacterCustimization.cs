a11ae66e2b5cd6c01e5d3517e2c267a03869bf78
David <Dankerprouduct2@gmail.com>
2015-10-15 21:18:06 -0400

    Database & Custimization
    added second set of armor to database

---
+++ CharacterCustimization.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class CharacterCustimization : MonoBehaviour {
+
+    ItemDatabase database;
+    ArmorPieces armorPiece;
+	// Use this for initialization
+	void Start () 
+    {
+        database = new ItemDatabase();
+        armorPiece = new ArmorPieces(); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        Helmet();
+        Torso();
+        RightBicep();
+        LeftBicep();
+        RightForearm();
+        LeftForearm();
+        Skirt();
+        RightThigh();
+        LeftThigh();
+        RightCalf();
+        LeftCalf();
+        RightFoot();
+        LeftFoot(); 
+
+    }
+
+    List<string> helmet = new List<string>();
+    void Helmet()
+    {
+
+        for (int i = 0; i < database.armor.Count; i++)
+        {
+           // if(database.armor[i])
+            
+        }
+    }
+    
+    List<string> torso = new List<string>();
+    void Torso()
+    {
+
+    }
+
+    List<string> rightBicep = new List<string>();
+    void RightBicep()
+    {
+
+    }
+
+    List<string> leftBicep = new List<string>(); 
+    void LeftBicep()
+    {
+        
+    }
+
+    List<string> rightForearm = new List<string>();
+    void RightForearm()
+    {
+
+    }
+
+    List<string> leftForearm = new List<string>();
+    void LeftForearm()
+    {
+
+    }
+
+    List<string> skirt = new List<string>();
+    void Skirt()
+    {
+
+    }
+
+    List<string> rightThigh = new List<string>(); 
+    void RightThigh()
+    {
+
+    }
+
+    List<string> leftthigh = new List<string>(); 
+    void LeftThigh()
+    {
+
+    }
+
+    List<string> rightCalf = new List<string>(); 
+    void RightCalf()
+    {
+
+    }
+
+    List<string> leftCald = new List<string>();
+    void LeftCalf()
+    {
+
+    }
+
+    List<string> rightFoot = new List<string>(); 
+    void RightFoot()
+    {
+
+    }
+
+    List<string> leftFoor = new List<string>(); 
+    void LeftFoot()
+    {
+
+    }
+
+}
--- ItemDatabase.cs
+++ ItemDatabase.cs
@@ -6,22 +6,50 @@ public class ItemDatabase : MonoBehaviour {
 
 
     public List<ArmorPieces> armor = new List<ArmorPieces>(); 
+    /// <summary>
+    ///  naming scheme
+    ///  101 
+    ///  - first set, first peice
+    ///  102
+    ///  - first set second peice
+    ///  
+    ///  404 
+    ///  - fourth set, fourth peice
+    ///  412
+    ///  - fourth set, 12 peice
+    /// </summary>
     void Awake()
     {
+        
         // basic set one
-        armor.Add(new ArmorPieces("Head", 0, ArmorPieces.ArmorType.Helmet, "Basic Helmet", 0, 0));
-        armor.Add(new ArmorPieces("Torso", 1, ArmorPieces.ArmorType.Torso, "Basic Torso", 0f, 0f));
-        armor.Add(new ArmorPieces("Right Bicep", 2, ArmorPieces.ArmorType.RightBicep, "Basic Bicep", 0, 0));
-        armor.Add(new ArmorPieces("Left Bicep", 3, ArmorPieces.ArmorType.LeftForearm, "Basic Bicep", 0, 0));
-        armor.Add(new ArmorPieces("Right Forearm", 4, ArmorPieces.ArmorType.RightForearm, "Basic Forearm", 0, 0));
-        armor.Add(new ArmorPieces("Left Forearm", 5, ArmorPieces.ArmorType.LeftForearm, "Basic Forearm", 0, 0));
-        armor.Add(new ArmorPieces("Right Calf", 6, ArmorPieces.ArmorType.RightCalf, "Basic Calf", 0, 0));
-        armor.Add(new ArmorPieces("Left Calf", 7, ArmorPieces.ArmorType.LeftCalf, "Basic Calf", 0, 0));
-        armor.Add(new ArmorPieces("Right Thigh", 8, ArmorPieces.ArmorType.RightThigh, "Basic Thigh", 0, 0));
-        armor.Add(new ArmorPieces("Left Thigh", 9, ArmorPieces.ArmorType.LeftThigh, "Basic Thigh", 0, 0));
-        armor.Add(new ArmorPieces("Skirt", 10, ArmorPieces.ArmorType.SkirtPiece, "Basic Thigh", 0, 0));
+        armor.Add(new ArmorPieces("Head", 101, ArmorPieces.ArmorType.Helmet, "Basic Helmet", 0, 0));
+        armor.Add(new ArmorPieces("Torso", 102, ArmorPieces.ArmorType.Torso, "Basic Torso", 0f, 0f));
+        armor.Add(new ArmorPieces("Right Bicep", 103, ArmorPieces.ArmorType.RightBicep, "Basic Bicep", 0, 0));
+        armor.Add(new ArmorPieces("Left Bicep", 104, ArmorPieces.ArmorType.LeftForearm, "Basic Bicep", 0, 0));
+        armor.Add(new ArmorPieces("Right Forearm", 105, ArmorPieces.ArmorType.RightForearm, "Basic Forearm", 0, 0));
+        armor.Add(new ArmorPieces("Left Forearm", 106, ArmorPieces.ArmorType.LeftForearm, "Basic Forearm", 0, 0));
+        armor.Add(new ArmorPieces("Right Calf", 107, ArmorPieces.ArmorType.RightCalf, "Basic Calf", 0, 0));
+        armor.Add(new ArmorPieces("Left Calf", 108, ArmorPieces.ArmorType.LeftCalf, "Basic Calf", 0, 0));
+        armor.Add(new ArmorPieces("Right Thigh", 109, ArmorPieces.ArmorType.RightThigh, "Basic Thigh", 0, 0));
+        armor.Add(new ArmorPieces("Left Thigh", 110, ArmorPieces.ArmorType.LeftThigh, "Basic Thigh", 0, 0));
+        armor.Add(new ArmorPieces("Skirt", 111, ArmorPieces.ArmorType.SkirtPiece, "Basic Thigh", 0, 0));
+        armor.Add(new ArmorPieces("Right Foot", 112, ArmorPieces.ArmorType.RightFoot, "Basic Foot", 0, 0));
+        armor.Add(new ArmorPieces("Left Foot", 113, ArmorPieces.ArmorType.LeftFoot, "Basic Foot", 0, 0)); 
         
         // Vindicator
+        armor.Add(new ArmorPieces("Vindicator Head", 201, ArmorPieces.ArmorType.Helmet, "Vindicator Helmet", 0, 0));
+        armor.Add(new ArmorPieces("Vindicator Torso", 202, ArmorPieces.ArmorType.Torso, "Vindicator Torso", 0, 0));
+        armor.Add(new ArmorPieces("Vindicator Right Bicep", 203, ArmorPieces.ArmorType.RightBicep, "Vindicator Right Bicep", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Left Bicep", 204, ArmorPieces.ArmorType.LeftBicep,"Vindicator Left Bicep", 0, 0,)); 
+        armor.Add(new ArmorPieces("Vindicator Right Forearm", 205, ArmorPieces.ArmorType.RightForearm,"Vindicator Right Forearm", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Left Forearm", 206, ArmorPieces.ArmorType.LeftForearm,"Vindicator Left Forearm", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Right Calf", 207, ArmorPieces.ArmorType.RightCalf, "Vindicator Right Calf", 0, 0));
+        armor.Add(new ArmorPieces("Vindicator Left Calf", 208, ArmorPieces.ArmorType.LeftCalf, "Vindicator Left Calf", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Right Thigh", 209, ArmorPieces.ArmorType.RightThigh, "Vindicator Right Thigh", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Left Thigh", 210, ArmorPieces.ArmorType.LeftThigh, "Vindicator Left Thigh", 0, 0));
+        armor.Add(new ArmorPieces("Vindicator Skirt", 211, ArmorPieces.ArmorType.SkirtPiece, "Vindicator Skirt  Piece", 0, 0));
+        armor.Add(new ArmorPieces("Vindicator Right Foot", 212, ArmorPieces.ArmorType.RightFoot, "Vindicator Right Foot", 0, 0)); 
+        armor.Add(new ArmorPieces("Vindicator Left Foot", 213, ArmorPieces.ArmorType.LeftFoot, "Vindicator Left Foot", 0, 0)); 
 
 
     }
